11fdac67d4be26c96a0f340c4c29244874242461
brian <brian.oliveira100@gmail.com>
2020-02-02 19:10:29 -0400

    Adicionado calendário semanal no frontend

--- frontend/src/assets/component/sala/sala.js
+++ frontend/src/assets/component/sala/sala.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Button from "../button/button";
 import "./styles.sass";
+import WeekCalendar from "../week_calendar/calendar";
 
 const Sala = props => {
     return (
-        <div className={'sala_container'}>
+        <div className={'sala_content'}>
+            <div className={'sala_container'}>
             <div>
                 <h1>Sala 01</h1>
                  <p>Horários Reservados</p>
@@ -12,6 +14,9 @@ const Sala = props => {
             <div>
                 <Button width={'25%'} text={'Detalhes'} />
             </div>
+            </div>
+            <WeekCalendar />
+
         </div>
     )
 };
--- frontend/src/assets/component/week_calendar/calendar.js
+++ frontend/src/assets/component/week_calendar/calendar.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import "./styles.sass";
+
+const days = new Array(7).fill(0);
 
 const WeekCalendar = props => {
     return (
-        <div>
-
+        <div className={'container_week'}>
+            {days.map(day => (
+                <div className={'week_day'}>
+                    <h1>Seg</h1>
+                    <h3>29/Jan</h3>
+                    <div><span>+</span></div>
+                </div>
+            ))}
         </div>
     )
 }
