f2c406df38b256f2dbc469b14dc75b58704ce1f8
jeliel <jeliel.augusto10@gmail.com>
2020-02-04 15:01:41 -0400

    Cliente e Reserva DAOs

--- frontend/src/DAO/clienteDAO.js
+++ frontend/src/DAO/clienteDAO.js
@@ -1,15 +1,30 @@
+import { Stitch, UserPasswordCredential, UserPasswordAuthProviderClient } from "mongodb-stitch-browser-sdk";
+
+const COLLECTION = 'clientes'
 const clienteDAO = {
+    db: null,
+    setDb(db){
+        this.db = db;
+    },
     create(cliente){
-
+        return this.db.collection(COLLECTION).insertOne(cliente);
     },
-    update(cliente){
-
+    update(query, changes){
+        return this.db.collection(COLLECTION).updateMany(query, {$set: changes});
     },
-    delete(id_cliente){
-        
+    editCliente(id_cliente, edits){
+        return this.update({_id: id_cliente}, edits);
+    },
+    async addUser(client, email, password, clienteData){
+        const emailPasswordClient = client.auth.getProviderClient(UserPasswordAuthProviderClient.factory);
+        await emailPasswordClient.registerWithEmail(email, password);
+        return this.create({ ...clienteData, email });
+    },
+    login(client, email, password){
+        return client.auth.loginWithCredential(new UserPasswordCredential(email, password));
     },
     findAll(){
-        
+        return this.db.collection(COLLECTION).find({}).toArray();
     }
 };
 export default clienteDAO;
