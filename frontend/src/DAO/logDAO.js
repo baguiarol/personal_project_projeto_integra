048461117644a6f40fd5d97adf6b544f4186ca36
Rayana <rannya7x@gmail.com>
2020-02-03 22:24:26 -0400

    logDAO

--- frontend/src/DAO/logDAO.js
+++ frontend/src/DAO/logDAO.js
@@ -1,9 +1,11 @@
-const logDAO = {
-    create(log){
+const COLLECTION = 'logs'
 
+const logDAO = {
+    create(db, log){
+        return db.collection(COLLECTION).insertOne(log);
     },
-    findAll(){
-        
+    findAll(db){
+        return db.collection(COLLECTION).find({}).toArray();
     }
 };
 export default logDAO;
