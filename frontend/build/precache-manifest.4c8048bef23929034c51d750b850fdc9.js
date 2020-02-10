ba999bf743c0ab1c6afabc36b6f934809f8b5c40
brian <brian.oliveira100@gmail.com>
2020-02-10 06:50:39 -0400

    Criado a build e pronto para merge. Finalizado somente o front desktop

---
+++ frontend/build/precache-manifest.4c8048bef23929034c51d750b850fdc9.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25a8654939246cc7b7234643b293c719",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab4b0680c4a05872c4bb",
+    "url": "/static/css/3.7affd829.chunk.css"
+  },
+  {
+    "revision": "079f1d693ee7864eeede",
+    "url": "/static/css/main.e884a7c2.chunk.css"
+  },
+  {
+    "revision": "ab4b0680c4a05872c4bb",
+    "url": "/static/js/3.66bffc67.chunk.js"
+  },
+  {
+    "revision": "36d5d1db5fb2a705158a3cc8c218bdc1",
+    "url": "/static/js/3.66bffc67.chunk.js.LICENSE"
+  },
+  {
+    "revision": "434a64debb84dec5c927",
+    "url": "/static/js/4.630c0bc3.chunk.js"
+  },
+  {
+    "revision": "1f511d5d7baea7a663db7666f6bcaff2",
+    "url": "/static/js/4.630c0bc3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "459a094c906b5aebecc5",
+    "url": "/static/js/5.e327862b.chunk.js"
+  },
+  {
+    "revision": "2d0f09986054323ae30e",
+    "url": "/static/js/6.29288543.chunk.js"
+  },
+  {
+    "revision": "671ed6adaa24d2bab84615ceeade0aa5",
+    "url": "/static/js/6.29288543.chunk.js.LICENSE"
+  },
+  {
+    "revision": "079f1d693ee7864eeede",
+    "url": "/static/js/main.1f422d54.chunk.js"
+  },
+  {
+    "revision": "16b3d7a1c6d0097acda9",
+    "url": "/static/js/runtime-main.52bec6e4.js"
+  },
+  {
+    "revision": "26b5da8ff8f46eeaae99",
+    "url": "/static/js/xlsx.78aae2e8.chunk.js"
+  },
+  {
+    "revision": "088a34f78f530102fd9661173b4a4f26",
+    "url": "/static/media/fa-brands-400.088a34f7.eot"
+  },
+  {
+    "revision": "273dc9bf9778fd37fa61357645d46a28",
+    "url": "/static/media/fa-brands-400.273dc9bf.ttf"
+  },
+  {
+    "revision": "822d94f19fe57477865209e1242a3c63",
+    "url": "/static/media/fa-brands-400.822d94f1.woff2"
+  },
+  {
+    "revision": "d72293118cda50ec50c39957d9d836d0",
+    "url": "/static/media/fa-brands-400.d7229311.svg"
+  },
+  {
+    "revision": "f4920c94c0861c537f72ba36590f6362",
+    "url": "/static/media/fa-brands-400.f4920c94.woff"
+  },
+  {
+    "revision": "3ac49cb33f43a6471f21ab3df40d1b1e",
+    "url": "/static/media/fa-regular-400.3ac49cb3.eot"
+  },
+  {
+    "revision": "9efb86976bd53e159166c12365f61e25",
+    "url": "/static/media/fa-regular-400.9efb8697.woff2"
+  },
+  {
+    "revision": "a57bcf76c178aee452db7a57b75509b6",
+    "url": "/static/media/fa-regular-400.a57bcf76.woff"
+  },
+  {
+    "revision": "d2e53334c22a9a4937bc26e84b36e1e0",
+    "url": "/static/media/fa-regular-400.d2e53334.svg"
+  },
+  {
+    "revision": "ece54318791c51b52dfdc689efdb6271",
+    "url": "/static/media/fa-regular-400.ece54318.ttf"
+  },
+  {
+    "revision": "2aa6edf8f296a43b32df35f330b7c81c",
+    "url": "/static/media/fa-solid-900.2aa6edf8.ttf"
+  },
+  {
+    "revision": "7a5de9b08012e4da40504f2cf126a351",
+    "url": "/static/media/fa-solid-900.7a5de9b0.svg"
+  },
+  {
+    "revision": "7fb1cdd9c3b889161216a13267b55fe2",
+    "url": "/static/media/fa-solid-900.7fb1cdd9.eot"
+  },
+  {
+    "revision": "93f284548b42ab76fe3fd03a9d3a2180",
+    "url": "/static/media/fa-solid-900.93f28454.woff"
+  },
+  {
+    "revision": "f6121be597a72928f54e7ab5b95512a1",
+    "url": "/static/media/fa-solid-900.f6121be5.woff2"
+  },
+  {
+    "revision": "ffdf0581a2ffb6ec5740e814887dccbf",
+    "url": "/static/media/integra_logo.ffdf0581.png"
+  },
+  {
+    "revision": "6f283a644c2d00abe26768981e23ede4",
+    "url": "/static/media/sala.6f283a64.png"
+  }
+]);
