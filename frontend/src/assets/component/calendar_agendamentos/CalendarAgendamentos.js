db05081811d3559b102cfa4ec32ef6371862c5fc
brian <brian.oliveira100@gmail.com>
2020-02-14 12:47:18 -0400

    Calendário do Agendamento do Administrador estruturado, falta SASS

---
+++ frontend/src/assets/component/calendar_agendamentos/CalendarAgendamentos.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import "./CalendarAgendamentos.sass";
+
+const fillHoras = () => {
+    let array = [];
+    for (let i = 0; i < 13; i++)
+        array.push(i + 8 + ':00');
+    return array;
+}
+
+const horas = fillHoras();
+const salas = new Array(8).fill('Sala 02');
+
+const CalendarAgendamentos = props => {
+    return (
+        <div className={'calendar_agendamentos_container'}>
+            <h1>
+                <span style={{float: 'left'}}><i className={'fa fa-chevron-left'}/> </span>
+                17 de Fevereiro de 2020
+                <span style={{float: 'right'}}><i className={'fa fa-chevron-right'}/> </span>
+            </h1>
+            <table className={'calendar_table'}>
+                {/*aqui vai a numeracao das salas*/}
+                <thead>
+                    <tr>
+                        <td></td>
+                        {
+                            salas.map(sala => <td>{sala}</td>)
+                        }
+                    </tr>
+                </thead>
+                <tbody>
+                {
+                    horas.map(hora => (
+                        <tr>
+                            <td>{hora}</td>
+                            { salas.map(sala => {
+                                if (Math.round(Math.random())) {
+                                    return (
+                                        <td>
+                                        <i className={'fa fa-plus'}/>
+                                        </td>
+                                    )
+                                } else {
+                                    return (<td>
+                                        Igor Nogueira
+                                    </td>)
+                                }
+                            })}
+                        </tr>
+                    ))
+                }
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default CalendarAgendamentos;
--- frontend/src/pages/administrador/agendamentos/index.js
+++ frontend/src/pages/administrador/agendamentos/index.js
@@ -3,13 +3,14 @@ import AdministradorTopbar from "../../../assets/component/adm_topbar/adm_topbar
 import WeekCalendar from "../../../assets/component/week_calendar/calendar";
 import Sala from "../../../assets/component/sala/sala";
 import ModalTypes from "../../../assets/modal_types";
+import CalendarAgendamentos from "../../../assets/component/calendar_agendamentos/CalendarAgendamentos";
 
 const AgendamentosAdministrador = props => {
     return (
         <div>
             <AdministradorTopbar pageSelected={'agendamentos'} />
             <div className={'container_salas'}>
-
+                <CalendarAgendamentos/>
             </div>
         </div>
     )
