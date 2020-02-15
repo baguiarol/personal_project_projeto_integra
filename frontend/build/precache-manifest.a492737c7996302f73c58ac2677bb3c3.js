85e6c2090d9fe4327697a64b2cd2d1423825d1fc
brian <brian.oliveira100@gmail.com>
2020-02-14 21:20:26 -0400

    Primeira build do projeto para o Hostgator e requisitos atualizados

---
+++ frontend/build/precache-manifest.a492737c7996302f73c58ac2677bb3c3.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bee5060e7f4b54cff817a87b94d329b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "26b5411dda259506c013",
+    "url": "./static/css/3.e2d11c62.chunk.css"
+  },
+  {
+    "revision": "983e8fef874ce75b8953",
+    "url": "./static/css/main.68a27c99.chunk.css"
+  },
+  {
+    "revision": "26b5411dda259506c013",
+    "url": "./static/js/3.11c3f0a1.chunk.js"
+  },
+  {
+    "revision": "36d5d1db5fb2a705158a3cc8c218bdc1",
+    "url": "./static/js/3.11c3f0a1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2d544d82bb82a54b7d01",
+    "url": "./static/js/4.42f8b5f1.chunk.js"
+  },
+  {
+    "revision": "1f511d5d7baea7a663db7666f6bcaff2",
+    "url": "./static/js/4.42f8b5f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "93335648a4c7d42dbccf",
+    "url": "./static/js/5.a2284bf4.chunk.js"
+  },
+  {
+    "revision": "c6cbc978720102ab0ec5",
+    "url": "./static/js/6.0f91e237.chunk.js"
+  },
+  {
+    "revision": "671ed6adaa24d2bab84615ceeade0aa5",
+    "url": "./static/js/6.0f91e237.chunk.js.LICENSE"
+  },
+  {
+    "revision": "983e8fef874ce75b8953",
+    "url": "./static/js/main.a4935a29.chunk.js"
+  },
+  {
+    "revision": "805dcda5d59ab54d9903",
+    "url": "./static/js/runtime-main.f83217a6.js"
+  },
+  {
+    "revision": "54d2fc21c53bc2e46785",
+    "url": "./static/js/xlsx.7de000f3.chunk.js"
+  },
+  {
+    "revision": "088a34f78f530102fd9661173b4a4f26",
+    "url": "./static/media/fa-brands-400.088a34f7.eot"
+  },
+  {
+    "revision": "273dc9bf9778fd37fa61357645d46a28",
+    "url": "./static/media/fa-brands-400.273dc9bf.ttf"
+  },
+  {
+    "revision": "822d94f19fe57477865209e1242a3c63",
+    "url": "./static/media/fa-brands-400.822d94f1.woff2"
+  },
+  {
+    "revision": "d72293118cda50ec50c39957d9d836d0",
+    "url": "./static/media/fa-brands-400.d7229311.svg"
+  },
+  {
+    "revision": "f4920c94c0861c537f72ba36590f6362",
+    "url": "./static/media/fa-brands-400.f4920c94.woff"
+  },
+  {
+    "revision": "3ac49cb33f43a6471f21ab3df40d1b1e",
+    "url": "./static/media/fa-regular-400.3ac49cb3.eot"
+  },
+  {
+    "revision": "9efb86976bd53e159166c12365f61e25",
+    "url": "./static/media/fa-regular-400.9efb8697.woff2"
+  },
+  {
+    "revision": "a57bcf76c178aee452db7a57b75509b6",
+    "url": "./static/media/fa-regular-400.a57bcf76.woff"
+  },
+  {
+    "revision": "d2e53334c22a9a4937bc26e84b36e1e0",
+    "url": "./static/media/fa-regular-400.d2e53334.svg"
+  },
+  {
+    "revision": "ece54318791c51b52dfdc689efdb6271",
+    "url": "./static/media/fa-regular-400.ece54318.ttf"
+  },
+  {
+    "revision": "2aa6edf8f296a43b32df35f330b7c81c",
+    "url": "./static/media/fa-solid-900.2aa6edf8.ttf"
+  },
+  {
+    "revision": "7a5de9b08012e4da40504f2cf126a351",
+    "url": "./static/media/fa-solid-900.7a5de9b0.svg"
+  },
+  {
+    "revision": "7fb1cdd9c3b889161216a13267b55fe2",
+    "url": "./static/media/fa-solid-900.7fb1cdd9.eot"
+  },
+  {
+    "revision": "93f284548b42ab76fe3fd03a9d3a2180",
+    "url": "./static/media/fa-solid-900.93f28454.woff"
+  },
+  {
+    "revision": "f6121be597a72928f54e7ab5b95512a1",
+    "url": "./static/media/fa-solid-900.f6121be5.woff2"
+  },
+  {
+    "revision": "ffdf0581a2ffb6ec5740e814887dccbf",
+    "url": "./static/media/integra_logo.ffdf0581.png"
+  },
+  {
+    "revision": "6f283a644c2d00abe26768981e23ede4",
+    "url": "./static/media/sala.6f283a64.png"
+  }
+]);
--- frontend/build/static/js/xlsx.78aae2e8.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpweb=this.webpackJsonpweb||[]).push([[2],{200:function(n,c){},206:function(n,c){},207:function(n,c){}}]);
-//# sourceMappingURL=xlsx.78aae2e8.chunk.js.map
