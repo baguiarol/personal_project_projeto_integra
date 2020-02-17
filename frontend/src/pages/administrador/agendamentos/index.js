a1e5e31d53f31d906cec3f977f0a58b897d4258a
brenda <brenda.aguiar@icomp.ufam.edu.br>
2020-02-17 14:48:21 -0400

    Responsividade de algumas telas (Ainda não finalizado)

--- frontend/src/pages/administrador/agendamentos/index.js
+++ frontend/src/pages/administrador/agendamentos/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import AdministradorTopbar from "../../../assets/component/adm_topbar/adm_topbar";
 import CalendarAgendamentos from "../../../assets/component/calendar_agendamentos/CalendarAgendamentos";
-
+//import 'agendamentos.sass'
 const AgendamentosAdministrador = props => {
     return (
         <div>
