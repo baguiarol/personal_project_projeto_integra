7be5236112641d71878ed1388bd062697ed9ad87
Brian <brian.oliveira100@gmail.com>
2020-02-19 18:50:49 -0400

    adding first php files to image upload

---
+++ frontend/build/upload_page.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+    <head>
+    <title>Teste de upload de imagem</title>
+    </head>
+    <body>
+        <form action='./imageUpload.php' enctype='multipart/form-data' method='POST'>
+            <input type='file' name='userfile'>
+            <input type='submit'>
+        </form>
+    </body>
+</html>
