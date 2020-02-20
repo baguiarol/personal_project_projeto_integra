c4867d41ad5c40ceb42d306e56d959ec88d42ce6
brian <brian.oliveira100@gmail.com>
2020-02-20 14:42:18 -0400

    Adicionado image Preview.

--- frontend/src/assets/component/file_input/FileInput.js
+++ frontend/src/assets/component/file_input/FileInput.js
@@ -7,10 +7,11 @@ const FileInput = props => {
 
     let realUploadButton = React.createRef();
     const [fileName, setFileName] = React.useState('Nenhum arquivo selecionado.');
+    const [fileURL, setFileURL] = React.useState('');
     const onChange = e => {
-        let x = realUploadButton.current.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/);
-        if (x) {
-            setFileName(x[1]);
+        if (e.target.files) {
+            setFileName(e.target.files[0].name);
+            setFileURL(URL.createObjectURL(e.target.files[0]))
         } else {
             setFileName('Nenhum arquivo selecionado.');
         }
@@ -19,38 +20,46 @@ const FileInput = props => {
     const [isURL, setURL] = React.useState(false);
 
     return (
-        <div className={'file_input_container'}>
-            <input type={'file'}
-                   name={'fileToUpload'}
-                   id={'real_file'}
-                   onChange={onChange}
-                   hidden
-                   accept={'image/png, image/jpeg, image/jpg'}
-                   ref={realUploadButton}/>
-            <div className={'buttons'}>
-                <button
-                    disabled={isURL}
-                    onClick={() => setURL(true)}
-                    className={isURL ? 'button_secondary selected' : 'button_secondary'}>
-                    URL
-                </button>
-                <button disabled={!isURL}
-                        onClick={() => setURL(false)}
-                        className={!isURL ? 'button_secondary selected' : 'button_secondary'}>
-                    Arquivo
-                </button>
+        <div className={'preview_input_container'}>
+            <div className={'file_input_container'}>
+                <img src={fileURL} />
+                <input type={'file'}
+                       name={'fileToUpload'}
+                       id={'real_file'}
+                       onChange={onChange}
+                       hidden
+                       accept={'image/png, image/jpeg, image/jpg'}
+                       ref={realUploadButton}/>
+                <div className={'buttons'}>
+                    <button
+                        disabled={isURL}
+                        onClick={() => setURL(true)}
+                        className={isURL ? 'button_secondary selected' : 'button_secondary'}>
+                        URL
+                    </button>
+                    <button disabled={!isURL}
+                            onClick={() => setURL(false)}
+                            className={!isURL ? 'button_secondary selected' : 'button_secondary'}>
+                        Arquivo
+                    </button>
+                </div>
+                <div className={'arquivo_upload'} style={!isURL ? {} : {display: 'none'}}>
+                    <button type={'button'} onClick={() => {
+                        realUploadButton.current.click();
+                    }}>
+                        <i className={'fa fa-upload'}/>
+                    </button>
+                    <span>{fileName}</span>
+                </div>
+                <InputText style={!isURL ? {display: 'none'} : {width: '100%'}} label={'URL da foto'}/>
             </div>
-            <div className={'arquivo_upload'} style={!isURL ? {} : {display: 'none'}}>
-                <button type={'button'} onClick={() => {
-                    realUploadButton.current.click();
-                }}>
-                    <i className={'fa fa-upload'} />
-                </button>
-                <span>{fileName}</span>
-            </div>
-            <InputText style={!isURL ? {display: 'none'} : {width: '100%'}} label={'URL da foto'}/>
         </div>
     )
+}
+
+FileInput.propTypes = {
+    data: PropTypes.any,
+    setData: PropTypes.func,
 }
 
 
